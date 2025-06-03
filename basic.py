0e8e6ecc8a5968baad2e31ea457173d1487fdb3c
Varun Singh <varunbaroliya034@gmail.com>
2025-06-03 11:57:57 +0530

    gen ai

---
+++ basic.py
@@ -0,0 +1,33 @@
+a = 10
+print(type(a))  # tell the type of the variable
+
+# strings - it is immutable / unchangable ( insert , update )
+# order is preserved 
+# string is indexed
+
+## ways we can denote the string?
+
+# three ways 
+# single , double , triple quote
+
+# single and double is same 
+
+# triple is used in multi line string
+
+a = "hEllo"
+print(type(a))
+
+print(a.upper())  # change the case of the string to capital
+
+print(a.capitalize())
+
+# print(a.count())
+
+print(a.casefold())
+
+print(a.lower())
+
+# print(a.index())
+
+
+
