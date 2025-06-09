cecff968ad5545d912116f41a99ae87780f31412
Varun Singh <varunbaroliya034@gmail.com>
2025-06-09 17:23:03 +0530

    app

--- Cli_based_chat_app/receiver.py
+++ Cli_based_chat_app/receiver.py
@@ -1,7 +1,8 @@
 import socket
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
+# sender ke ander receiver ka ip
+# receiver ke ander sender ka ip
 ip_add = '192.168.72.36'
 port_num = 7000   # 0--65536
 complete_add = (ip_add,port_num)
